b41b43fdf018b9f6c3b662eb6777755aeb29991e
Rob Tillaart <rob.tillaart@gmail.com>
2024-03-27 19:10:48 +0100

    add registerDumper.ino

--- examples/KT0803_register_dumper/KT0803_register_dumper.ino
+++ examples/KT0803_register_dumper/KT0803_register_dumper.ino
@@ -4,23 +4,30 @@
 // PURPOSE: debugger
 //     URL: https://github.com/RobTillaart/TinyKT0803
 //
-//  one must make the protected function public to have this work.
+//  one must make the protected functions public to have this work.
 
 #include "TinyKT0803.h"
-#include "SoftwareSerial.h"
+#include <Tiny4kOLED.h>
 
 TinyKT0803 FM_SEND;
 
-uint8_t tx = 3;
-uint8_t rx = 255;  //  not needed here
+void oledInit() 
+{
+  oled.begin(128, 64, sizeof(tiny4koled_init_128x64br), tiny4koled_init_128x64br);
+  oled.setFont(FONT8X16P);
+  oled.on();
+}
 
-SoftwareSerial ser(tx, rx);
-
+void oledDebug(uint8_t reg) 
+{
+  oled.clear();
+  oled.setCursor(20, 2);
+  oled.print(FM_SEND.readData(reg));
+}
 
 void setup()
 {
-  ser.begin(9600);
-  ser.println("\nKT0803_register_dump\n");
+  oledInit();
 
   TinyWireM.begin();
   //  give I2C time to initialize
@@ -30,39 +37,16 @@ void setup()
   FM_SEND.setChannel(2000);  // * 0.05 = 100.00 MHz
   FM_SEND.setMute(false);
 
-  FM_SEND.setChannel(2000);
-  registerDump();
-  FM_SEND.setChannel(2001);
-  registerDump();
-  FM_SEND.setChannel(2002);
-  registerDump();
-  FM_SEND.setChannel(2003);
-  registerDump();
-  FM_SEND.setChannel(2004);
-  registerDump();
-  FM_SEND.setChannel(2005);
-  registerDump();
+  oledDebug(0);
+  delay(3000);
+  oledDebug(1);
+  delay(3000);
+  oledDebug(2); 
 }
 
 void loop()
 {
 }
 
-
-void registerDump()
-{
-  ser.println();
-  for (int reg = 0x00; reg <= 0x16; reg++)
-  {
-    int data = FM_SEND.readData(reg);
-    ser.print(reg);
-    ser.print("\t");
-    if (data < 0x10) ser.print("0");
-    ser.print(data, HEX);
-    ser.print("\t");
-    ser.print(data, BIN);
-    ser.println();
-  }
-}
 
 //  -- END OF FILE --
